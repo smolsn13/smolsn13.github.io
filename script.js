c00a0229bdd7878694023986dbcae5793bcd3d1d
Scott Molsness <smolsness@gmail.com>
2018-05-01 11:09:13 -0700

    added landing page with animation, sticky navbar and updated background color to gray

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+var mn = $(".main-nav");
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
+    $(window).scroll(function(){
+      if ( $(this).scrollTop() > 1440 ) {
+        mn.addClass("nav-scrolled")
+      }
+      else {
+        mn.removeClass("nav-scrolled")
+      }
+    });
   });
